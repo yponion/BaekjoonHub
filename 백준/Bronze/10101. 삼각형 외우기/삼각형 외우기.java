d4b842bc920a99141984e4ebac82f7cf8a9d0ba9
yponion <84820269+yponion@users.noreply.github.com>
2023-07-26 17:37:03 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 212 ms, Memory: 17452 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        checkTriangle(a, b, c);
+    }
+
+    public static void checkTriangle(int a, int b, int c) {
+        if (a == 60 && b == 60 && c == 60) {
+            System.out.println("Equilateral");
+        } else if ((a+b+c)==180) {
+            if(a==b||a==c||b==c){
+                System.out.println("Isosceles");
+            }else{
+                System.out.println("Scalene");
+            }
+        }else{
+            System.out.println("Error");
+        }
+    }
+}
