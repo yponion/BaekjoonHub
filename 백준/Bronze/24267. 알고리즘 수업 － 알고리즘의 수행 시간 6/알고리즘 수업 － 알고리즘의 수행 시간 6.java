5df1bbf567c8484277497386a6e0729dbce9f318
yponion <84820269+yponion@users.noreply.github.com>
2023-08-01 21:10:38 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(((n-2)*(n-1)*n)/6);
+        System.out.println(3);
+    }
+}
