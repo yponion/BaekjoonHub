42c6eb5fc1920948dbf4624f9b090f9696fd53c0
yponion <84820269+yponion@users.noreply.github.com>
2023-08-20 00:15:28 +0900

    [Silver IV] Title: 카드2, Time: 136 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -7,26 +7,41 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Integer> queue = new LinkedList<>();
         int n = 0;
         try {
             n = Integer.parseInt(br.readLine());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        for (int i = 1; i <= n; i++) {
-            queue.add(i);
-        }
-        boolean flag = true;
-        while (queue.size() > 1) {
-            if (flag) {
-                queue.poll();
-                flag = false;
-            } else {
-                queue.add(queue.poll());
-                flag = true;
+
+        if (n == 1) {
+            System.out.println(1);
+        } else {
+            int pow = 1;
+            while (n > Math.pow(2, pow)) {
+                pow++;
             }
+            System.out.println(Math.round((n - Math.pow(2, pow - 1)) * 2));
         }
-        System.out.println(queue.peek());
+
+//        //규칙 찾기
+//        Queue<Integer> queue = new LinkedList<>();
+//        for (int i = 1; i < 100; i++) {
+//            queue.clear();
+//            for (int j = 1; j <= i; j++) {
+//                queue.add(j);
+//            }
+//            boolean flag = true;
+//            while (queue.size() > 1) {
+//                if (flag) {
+//                    queue.poll();
+//                    flag = false;
+//                } else {
+//                    queue.add(queue.poll());
+//                    flag = true;
+//                }
+//            }
+//            System.out.println(i + " : " + queue.peek());
+//        }
     }
 }
