05480c413ae54657c22aa8b16cb171def7f6db6e
Jeongun Yang <84820269+yponion@users.noreply.github.com>
2024-02-14 20:24:20 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 276 ms, Memory: 26224 KB -BaekjoonHub

--- 백준/Silver IV/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver IV/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.HashSet;
 
 public class Main {
@@ -10,29 +12,32 @@ public class Main {
 
     public static void process() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         HashSet<String> set = new HashSet<>();
-        int cnt = 0;
         String input = "";
+        int n, cnt = 0;
+
         try {
             n = Integer.parseInt(br.readLine());
-            for (int i = 0; i <= n; i++) {
-                if (i == n) {
-                    input = "ENTER";
-                } else {
-                    input = br.readLine();
-                }
+            for (int i = 0; i < n; i++) {
+                input = br.readLine();
                 // 입력 받으며 ENTER인지 확인, ENTER가 아니라면 set에 추가, ENTER가 맞다면 set의 크기를 계산하고 clear
                 if (input.equals("ENTER")) {
-                    cnt += set.size();
                     set.clear();
                 } else {
-                    set.add(input);
+                    // set에 있는 값을 추가하면 false를 반환하는 것을 이용
+                    if (set.add(input)) {
+                        cnt++;
+                    }
                 }
             }
+            br.close();
+
+            bw.write(String.valueOf(cnt));
+            bw.flush();
+            bw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        System.out.print(cnt);
     }
 }
