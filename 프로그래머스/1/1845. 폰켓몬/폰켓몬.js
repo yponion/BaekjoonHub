cc6a6b1ec8db7e4d1a7c652f5f81d60a5c3e85d6
Jeongun Yang <84820269+yponion@users.noreply.github.com>
2024-08-29 18:28:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.62 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,12 +1,8 @@
 function solution(nums) {
+  const set = new Set(nums);
+
   const n = nums.length / 2;
+  const size = set.size;
 
-  const map = new Map();
-
-  nums.forEach((v) => {
-    if (map.get(v)) map.set(v, map.get(v) + 1);
-    else map.set(v, 1);
-  });
-
-  return map.size < n ? map.size : n;
+  return size < n ? size : n;
 }
